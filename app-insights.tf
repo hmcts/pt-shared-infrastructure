6ee12f966e15505f0992e8783cc018f372c3eb4e
hanimao <hmao3@outlook.com>
2026-04-01 14:09:56 +0100

    add connection string

--- app-insights.tf
+++ app-insights.tf
@@ -5,4 +5,11 @@ module "application_insights" {
 
   resource_group_name = azurerm_resource_group.rg.name
   common_tags         = var.common_tags
+  alert_limit_reached = true
+}
+
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = module.application_insights.connection_string
+  key_vault_id = module.key_vault.key_vault_id
 }
