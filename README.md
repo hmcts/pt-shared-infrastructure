585478ca9c098132f17063710f053dcb6c8d0c54
hanimao <hmao3@outlook.com>
2026-04-01 10:55:58 +0100

    add shared-infra components

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # rpt-shared-infrastructure
 
+Contains the product infrastructure components per Environment for Residential Property Tribunal
+
+Resource Group
+Azure Key Vault
---
+++ app-insights.tf
@@ -0,0 +1,10 @@
+module "application_insights" {
+  source  = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
+  env     = var.env
+  product = var.product
+
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "other"
+  common_tags         = var.common_tags
+  alert_limit_reached = true
+}
