81991f601d01bb9219ea64f34ad968df5e76c828
hanimao <hmao3@outlook.com>
2026-04-01 16:11:17 +0100

    add connection string

--- app-insights.tf
+++ app-insights.tf
@@ -12,4 +12,5 @@ resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = module.application_insights.connection_string
   key_vault_id = module.key_vault.key_vault_id
+
 }
