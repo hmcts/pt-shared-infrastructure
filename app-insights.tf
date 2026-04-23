f54dfbc0d31af813ebc81a7c955adcde5c31abd4
hanimao <hmao3@outlook.com>
2026-04-23 14:17:04 +0100

    fix infra

--- app-insights.tf
+++ app-insights.tf
@@ -1,18 +1,18 @@
-module "application_insights" {
-  source   = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
-  env      = var.env
-  product  = var.product
-  location = var.location
+# module "application_insights" {
+#   source   = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
+#   env      = var.env
+#   product  = var.product
+#   location = var.location
 
-  resource_group_name = azurerm_resource_group.rg.name
-  application_type    = "other"
-  common_tags         = var.common_tags
-  alert_limit_reached = true
-}
+#   resource_group_name = azurerm_resource_group.rg.name
+#   application_type    = "other"
+#   common_tags         = var.common_tags
+#   alert_limit_reached = true
+# }
 
-resource "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "app-insights-connection-string"
-  value        = module.application_insights.connection_string
-  key_vault_id = module.key-vault.key_vault_id
+# resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+#   name         = "app-insights-connection-string"
+#   value        = module.application_insights.connection_string
+#   key_vault_id = module.key-vault.key_vault_id
 
-}
+# }
