e513f9250dcd896e61d549d3f0e3d08d077f2590
Damon Green <damon.green@btinternet.com>
2026-04-29 15:20:36 +0100

    Quote

--- key-vault.tf
+++ key-vault.tf
@@ -1,6 +1,6 @@
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product                 = ${var.product}-kv-
+  product                 = "${var.product}-kv"
   env                     = var.env
   object_id               = var.jenkins_AAD_objectId
   resource_group_name     = azurerm_resource_group.rg.name
