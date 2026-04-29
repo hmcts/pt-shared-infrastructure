5885b1b945ce46705791da8c282528c49b3030f4
Damon Green <damon.green@btinternet.com>
2026-04-29 15:15:02 +0100

    Rename KV

--- key-vault.tf
+++ key-vault.tf
@@ -1,6 +1,6 @@
 module "key-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  product                 = var.product
+  product                 = ${var.product}-kv-
   env                     = var.env
   object_id               = var.jenkins_AAD_objectId
   resource_group_name     = azurerm_resource_group.rg.name
