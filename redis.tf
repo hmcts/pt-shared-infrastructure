1d3b230e5af213c5201ea56236f13bec205a4be0
hanimao <hmao3@outlook.com>
2026-06-15 11:27:15 +0100

    added component to be redis

--- redis.tf
+++ redis.tf
@@ -2,7 +2,7 @@ module "pt_managed_redis" {
   source = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
 
   product     = var.product
-  name        = "redis"
+  component   = "redis"
   env         = var.env
   location    = var.location
   common_tags = var.common_tags
