c61bed95f0009e43add562b3cde102109c786e40
hanimao <Hani.Mao@HMCTS.NET>
2026-09-24 16:07:12 +0100

    update branch to master

--- key-vault.tf
+++ key-vault.tf
@@ -1,5 +1,5 @@
 module "key-vault" {
-  source                       = "git@github.com:hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
+  source                       = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   product                      = var.product
   env                          = var.env
   name                         = "${var.product}-kv1-${var.env}"
